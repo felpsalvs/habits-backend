f73e031002ea29dfbbaf4b1783918d48741b3eac
Felipe Alves <profelipealves@outlook.com>
2023-01-27 17:03:15 -0300

    :sparkles: feat: add web-push

---
+++ src/notifications-routes.ts
@@ -0,0 +1,9 @@
+import { FastifyInstance } from "fastify";
+import WebPush from "web-push";
+
+const publicKey = 'BD2zEDbRWweZZvbE4Gx91hp451Qb1lV-NdOUbYsU8OfUpfVdyOsRSLtAelPy8r5YP_GUPivuuRdVXqm2ktQy3vg'
+const privateKey = 'RCESxhkplLvEwIsBYVmSWkXXyr9SLaPOCems6R8yovc'
+
+export async function notificationRoutes(app: FastifyInstance) {
+
+}
