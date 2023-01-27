ff25e6c7b964a93259a96fa9dbda33f91baa8c1a
Felipe Alves <profelipealves@outlook.com>
2023-01-27 17:51:58 -0300

    feat: add notification

--- src/notifications-routes.ts
+++ src/notifications-routes.ts
@@ -1,27 +1,42 @@
 import { FastifyInstance } from "fastify";
 import WebPush from "web-push";
+import { z } from "zod";
 
-const publicKey = 'BD2zEDbRWweZZvbE4Gx91hp451Qb1lV-NdOUbYsU8OfUpfVdyOsRSLtAelPy8r5YP_GUPivuuRdVXqm2ktQy3vg'
-const privateKey = 'RCESxhkplLvEwIsBYVmSWkXXyr9SLaPOCems6R8yovc'
+const publicKey =
+  "BD2zEDbRWweZZvbE4Gx91hp451Qb1lV-NdOUbYsU8OfUpfVdyOsRSLtAelPy8r5YP_GUPivuuRdVXqm2ktQy3vg";
+const privateKey = "RCESxhkplLvEwIsBYVmSWkXXyr9SLaPOCems6R8yovc";
 
-WebPush.setVapidDetails('http://localhost:3000', publicKey, privateKey)
+WebPush.setVapidDetails("http://localhost:3000", publicKey, privateKey);
 
 export async function notificationRoutes(app: FastifyInstance) {
-  app.get('/push/public_key', () => {
+  app.get("/push/public_key", () => {
     return {
       publicKey,
-    }
-})
+    };
+  });
 
-  app.post('/push/register', (request, reply) => {
-    console.log(request.body)
+  app.post("/push/register", (request, reply) => {
+    console.log(request.body);
+    return reply.status(201).send();
+  });
 
-    return reply.status(201).send()
-  })
+  app.post("/push/send", async (request, reply) => {
+    const sendPushBody = z.object({
+      subscription: z.object({
+        endpoint: z.string(),
+        keys: z.object({
+          p256dh: z.string(),
+          auth: z.string(),
+        }),
+      }),
+    });
 
-  app.post('/push/send', async (request, reply) => {
-    console.log(request.body)
+    const { subscription } = sendPushBody.parse(request.body);
 
-    return reply.status(201).send()
-  })
+    setTimeout(() => {
+      WebPush.sendNotification(subscription, "Hello from the server");
+    }, 5000);
+
+    return reply.status(201).send();
+  });
 }
